c960d68d2459bc9874c70076763dc5f86354f31b
Dexter Zacour <cdzacour@hotmail.com>
2022-05-16 15:55:11 -0400

    begining js

---
+++ assets/js/game.js
@@ -0,0 +1,14 @@
+function fight () {
+    window.alert("the fight has begun!");
+}
+//fight();
+
+var playerName = window.prompt("what is your robots name?");
+
+console.log(playerName);
+
+console.log("this logs a string, good for leaving yourself a message");
+
+console.log(10+10);
+
+console.log("our robots name is " + playerName);
